7b83cf7a3523dfe155e2753fe73aabdb30df9dea
abdoelbah <115045528+abdoelbah@users.noreply.github.com>
2023-12-27 09:52:11 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const router = require('./router/router');
 const cors = require('cors');
-const {validation} = require('./middleware/check');
+const {checkName} = require('./middleware/check');
 
 const app = express();
 
 app.use(cors())
 app.use(express.urlencoded({extended : true}))
 
-app.use(validation);
+app.use(checkName);
 app.use(router)
 app.listen(3000 , () => {
     console.log("listening on port 3000 🚀 ")
