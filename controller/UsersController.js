ecd39bb5ec5ebfb4eaf85ff7e4fad9a6eade345c
abdoelbah <115045528+abdoelbah@users.noreply.github.com>
2023-12-27 09:53:44 +0200

    Update UsersController.js

--- controller/UsersController.js
+++ controller/UsersController.js
@@ -1,23 +1,18 @@
 
 
 exports.LoginUser = (req, res) => {
-    const username = req.body.username;
-    if(username.trim === ""){
-        res.json({
-            msg : "الرجاء التأكد من ادخال البيانات",
-            state : 0
-        })
-    }
     res.json({
         msg: "تم تسجيلك بنجاح",
         state: 1
     })
 
-
 }
 
 exports.SignUpUser = (req, res) => {
-
+    res.json({
+        msg: "تم تسجيلك بنجاح",
+        state: 1
+    })
 
 }
 
