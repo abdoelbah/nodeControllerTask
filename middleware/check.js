f56c1081c040d7654b9ee583fea2925ea570c3d5
abdoelbah <115045528+abdoelbah@users.noreply.github.com>
2023-12-27 08:14:01 +0200

    Update check.js

--- middleware/check.js
+++ middleware/check.js
@@ -1,45 +1,24 @@
 
- exports.check = (req, res, next) => {
-    const name = req.body.name;
-
-    if( name === "rashad" || name === "abdo"|| name === "zakaria"|| name === "feda"){
-        next();
-        return;
-    }
-    res.json({
-        msg: "أنتَ غير مصرح لعمل هذه العملية"
-    })
-    
- }
-
-   exports.validation = ( req, res, next) =>{
+exports.checkName = (req, res, next) => {
     const username = req.body.username;
-    const password = req.body.password;
+        if(username === "abdo"){
+            next();
+            return
+        }
 
-    if (username.trim() === "" || password.trim() === "") {
-        return res.json({
-            msg: 'الرجاء ادخال البيانات',
-            state: 0
+        res.json({
+            msg : "غير مسموح لك باجراءالعملية"
+        })
+}
+
+exports.validation = (req, res, next) => {
+    const username = req.body.username;
+    if(username === "zakaria"){
+        res.json({
+            msg : "لست مصرحا بالدخول",
         })
     }
 
-    if (username.trim() === "zakraia" && password.trim() === "12345") {
-        next();
-        return
-    }
-    
-
-    if (username.trim() !=="zakraia" && password.trim() !== "12345") {
-        return res.json({
-            msg:" انتَ غير مصرح لهذه العملية",
-            state: 0
-        })
-
-    }
-
-   }
-
-
-
-
-
+    next();
+    return;
+}
