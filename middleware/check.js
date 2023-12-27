fe8f30e49523ec5e086ac12dee64cd1ffb42d23b
abdoelbah <115045528+abdoelbah@users.noreply.github.com>
2023-12-27 09:52:39 +0200

    Update check.js

--- middleware/check.js
+++ middleware/check.js
@@ -1,24 +1,28 @@
 
 exports.checkName = (req, res, next) => {
     const username = req.body.username;
-        if(username === "abdo"){
+        if(username === "abdo" || username ==="zakaria" || username === "feda"){
             next();
             return
         }
-
         res.json({
-            msg : "غير مسموح لك باجراءالعملية"
+            msg : "غير مسموح لك باجراءالعملية",
+            state : 0
         })
 }
 
 exports.validation = (req, res, next) => {
     const username = req.body.username;
-    if(username === "zakaria"){
-        res.json({
-            msg : "لست مصرحا بالدخول",
-        })
+    const password = req.body.password;
+    if(username === "zakaria" && password === "12345"){
+        next();
+        return;
     }
 
-    next();
-    return;
+    res.json({
+        msg : "غير مصرح لك بالدخول",
+        state : 0
+    })
+
+ 
 }
